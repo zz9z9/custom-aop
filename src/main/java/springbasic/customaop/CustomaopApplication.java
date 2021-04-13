e04557de00e1b7c92a1614de3ca6812e302da76c
Jaeyoon <wodbs4338@naver.com>
2021-04-14 00:28:56 +0900

    테스트 위한 최소한의 파일 세팅

---
+++ src/main/java/springbasic/customaop/CustomaopApplication.java
@@ -0,0 +1,13 @@
+package springbasic.customaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomaopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbasic/customaop/CustomaopApplicationTests.java
@@ -0,0 +1,13 @@
+package springbasic.customaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomaopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
